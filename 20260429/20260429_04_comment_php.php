52db555569844984826f09b8dadbfdefe75f3362
lina <a88091500@gmail.com>
2026-04-29 11:08:49 +0800

    php 註解

---
+++ 20260429/20260429_04_comment_php.php
@@ -0,0 +1,60 @@
+<?php
+// 單行註解
+    echo "Hello World!";
+
+    // 多行註解 手動 /* */
+
+    /*
+    echo "Hello World!";
+    echo "Hello World!";
+    echo "Hello World!";
+    */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        /* css 單行註解 */
+        h2 {
+            /* color: lightblue; */
+            text-align: center;
+        }
+
+        /* css 多行註解 */
+        h3 {
+            /* color: lightblue;
+            text-align: center; */
+        }
+    </style>
+</head>
+
+<body>
+
+    <h2>
+        文字單行註解 ctrl + /
+    </h2>
+    <p>
+        <!-- 文字單行註解 -->
+    </p>
+
+    <h3>
+        多行單行註解
+    </h3>
+    <p>
+        <!-- Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet. -->
+    </p>
+
+
+
+</body>
+
+</html>
